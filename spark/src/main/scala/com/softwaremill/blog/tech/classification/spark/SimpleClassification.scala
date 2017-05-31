5b1c88990ffdfd25ff66c2689a35c3f334a91414
Paweł Stawicki <pawel.stawicki@softwaremill.com>
2017-05-31 14:07:31 +0200

    First stab, created SparkContext and run simple jobs on text file

---
+++ spark/src/main/scala/com/softwaremill/blog/tech/classification/spark/SimpleClassification.scala
@@ -0,0 +1,16 @@
+package com.softwaremill.blog.tech.classification.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SimpleClassification {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local[4]").setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+    val abstracts = sc.textFile("data/abstracts.csv")
+    val numAs = abstracts.filter(line => line.contains("a")).count()
+    val numBs = abstracts.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    sc.stop()
+  }
+}
